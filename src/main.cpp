c47e5599237abc007534ea4be9320314f7225fe9
Nikolay Hizanishvili <nikolay.hizanishvili@gmail.com>
2022-02-14 20:40:33 +0300

    Add arg parsing

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,26 @@
+#include <string>
+#include <iostream>
+#include "FileStream.hpp"
+#include "tclap/CmdLine.h"
+
 int main(int argc, char** argv)
 {
+	try { 
 
+        // Define command line object 
+        TCLAP::CmdLine cmd("Analogue of the tee command", ' ', "0.1");
+        
+        // Set up arguments
+        TCLAP::UnlabeledValueArg<std::string> filenameArg("filename","Path to a file", true, "file", "string", cmd);
+        TCLAP::SwitchArg reverseSwitch("a","append","Appends input to existing file", cmd, false);
+
+        // Parse arguments 
+        cmd.parse( argc, argv );
+        std::string filename = filenameArg.getValue();
+        bool appendFlag = reverseSwitch.getValue();
+
+
+    } catch (TCLAP::ArgException &e) { // catch exceptions 
+            std::cerr << "error: " << e.error() << " for arg " << e.argId() << std::endl; 
+    }
 }
